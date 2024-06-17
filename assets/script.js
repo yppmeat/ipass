b15e35c369788e11ff297aa1c7ef634e1bf6cfa2
yppmeat <149466893+yppmeat@users.noreply.github.com>
2024-06-17 15:18:12 +0900

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,36 @@
-loadData();
+let DATA;
+
+fetch('/data/data.json')
+.then(res => res.json())
+.then(res => {
+  DATA = res;
+  dataLoadedHandler();
+});
+
+let q;
+function dataLoadedHandler() {
+  function onClickHandler() {
+    reload();
+  }
+  q = htmlv`
+    <div>
+      <div *as="key"></div>
+      <div *as="value"></div>
+      <input type="button" value="更新" *onclick=${onClickHandler}>
+    </div>
+  `;
+  document.body.append(...q);
+}
+
+function reload() {
+  const dataCopy = [...DATA];
+  const selectedValue = dataCopy.splice(random(dataCopy.length), 1)[0];
+
+  console.log(q.key, q.value, selectedValue);
+  q.key.innerText = selectedValue[0];
+  q.value.innerText = selectedValue[1];
+}
+
+function random(len) {
+  return Math.round(Math.random() * (len - 1));
+}
