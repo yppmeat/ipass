2221dbcef748ad6abc0b0c73dd5ef6be00cb0b5c
yppmeat <149466893+yppmeat@users.noreply.github.com>
2024-06-18 14:33:28 +0900

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -20,20 +20,35 @@ function dataLoadedHandler() {
     <div *as="question" class="question"></div>
     <div class="select">
       <div>
-        <input type="button" value="ア">
+        <input type="button" value="ア" *onclick=${ansClick}>
         <span *as="ans0"></span>
       </div>
       <div>
-        <input type="button" value="イ">
+        <input type="button" value="イ" *onclick=${ansClick}>
         <span *as="ans1"></span>
       </div>
       <div>
-        <input type="button" value="ウ">
+        <input type="button" value="ウ" *onclick=${ansClick}>
         <span *as="ans2"></span>
       </div>
       <div>
-        <input type="button" value="エ">
+        <input type="button" value="エ" *onclick=${ansClick}>
         <span *as="ans3"></span>
+      </div>
+    </div>
+    <div class="info">
+      <div>
+        <h3>分類</h3>
+        <span>テクノロジ系 » セキュリティ</span>
+      </div>
+      <div>
+        <h3>正解</h3>
+        <input type="button" value="正解を表示する" *onclick=${showAnswer} *as="ansBtn">
+        <span class="ans" *as="ans"></span><span class="ansSelected" *as="ansSelected"></span>
+      </div>
+      <div>
+        <h3>解説</h3>
+        <span></span>
       </div>
     </div>
     <div class="footerBtn">
@@ -46,13 +61,23 @@ function dataLoadedHandler() {
   reload();
 }
 
+function ansClick(e) {
+  
+}
+
+function showAnswer(e) {
+  e.target.style.display = 'none';
+  q.ans.innerText = ['ア', 'イ', 'ウ', 'エ'][ansIndex];
+}
+
+let ansIndex;
 function reload() {
   const { result: [question], arr: dataCopy } = selectFromArr(DATA, 1);
   const answer = selectFromArr(dataCopy, 3).result;
-  const ansIndex = random(4);
+  ansIndex = random(4);
   answer.splice(ansIndex, 0, question);
   
-  q.number0.innerText = 'ITパスポート単語集 問' + i;
+  q.number0.innerText = 'ITパスポート用語集 問' + i;
   q.number1.innerText = '問' + i;
   
   q.question.innerText = question[0] + 'についての説明はどれか。';
@@ -61,6 +86,9 @@ function reload() {
     q['ans' + i].innerText = v[1];
   });
   
+  q.ansBtn.style.display = 'block';
+  q.ans.innerText = '';
+
   i++;
 }
 
